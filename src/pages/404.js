b48759c5ccca07cf45b32a8889be20908d934506
Raj <therajtiwari254@gmail.com>
2021-10-13 23:59:26 +0530

    fixed the 404 page styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,27 +1,54 @@
 import React from "react";
 //components and pages
-import Home from "./pages/Home";
-import PageNotFound from "./pages/404";
-import GlobalStyles from "./components/GlobalStyles";
-import { Switch, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-function App() {
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+function PageNotFound() {
     return (
-        <div>
+        <StyledPageNotFound>
+            <div id="main">
+                <div class="fof">
+                    <h1>Error 404</h1>
+                </div>
+            </div>
+        </StyledPageNotFound >
 
-            <GlobalStyles />
-            <Switch>
-
-                <Route exact path={["/", "/game/:id"]}>
-
-                    <Home />
-                </Route>
-                <Route path="">
-                    <PageNotFound />
-                </Route>
-            </Switch>
-        </div>
     );
 }
 
-export default App;
+const StyledPageNotFound = styled(motion.div)`
+*{
+    transition: all 0.6s;
+}
+
+body{
+    font-family: 'Lato', sans-serif;
+    color: #888;
+    margin: 0;
+}
+
+#main{
+    display: table;
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+}
+
+.fof{
+	  display: table-cell;
+	  vertical-align: middle;
+}
+
+.fof h1{
+	  font-size: 50px;
+	  display: inline-block;
+	  padding-right: 12px;
+	  animation: type .5s alternate infinite;
+}
+
+@keyframes type{
+	  from{box-shadow: inset -3px 0px 0px #888;}
+	  to{box-shadow: inset -3px 0px 0px transparent;}
+}
+        `
+export default PageNotFound;
